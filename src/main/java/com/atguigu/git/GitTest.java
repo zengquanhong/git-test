8f8caccf7f30ad328c2c833d06e380854b3c911e
zengquanhong <122118225@qq.com>
2022-06-21 22:27:45 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,6 +2,9 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+
+
     }
 }
